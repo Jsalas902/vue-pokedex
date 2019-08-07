3d46cb65eb5f84c9f5cc2afa03e77aa1fdf51961
jainner salas <jsalas902@gmail.com>
2019-08-06 22:43:47 -0400

    cargando pokemons con fetch y creando el evento de eliminar, correcciones del css material Icons

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-var app = new Vue({
+const app = new Vue({
     el: '#app',
-    data: {
-        pokemons: pokemons,
-        typeColor: {
-            "grass": "#78C850",
-            "poison": "#A040A0",
-            "fire": "#F08030",
-            "flying": "#A890F0",
-            "water": "#6890F0",
-            "bug": "#A8B820",
-            "normal": "#A8A878",
-            "electric": "#F8D030",
+    data() {
+        return {
+            pokemons: [],
+            typeColor: {
+                "grass": "#78C850",
+                "poison": "#A040A0",
+                "fire": "#F08030",
+                "flying": "#A890F0",
+                "water": "#6890F0",
+                "bug": "#A8B820",
+                "normal": "#A8A878",
+                "electric": "#F8D030",
+            }
         }
+    },
+    methods: {
+        removePokemon(pokemonToDelete) {
+            this.pokemons = this.pokemons
+                .filter(pokemon => pokemon !== pokemonToDelete);
+        }
+    },
+    created() {
+        fetch('data/pokemons.json')
+            .then(response => response.json())
+            .then(pokemons => this.pokemons = pokemons);
     }
-})
+});
